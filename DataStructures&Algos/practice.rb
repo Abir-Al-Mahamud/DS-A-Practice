090da95a123b3e999890030c436cf9e61aef31c8
your name here <abirmahamud610@gmail.com>
2022-02-23 12:23:34 -0500

    Work on algorithms

--- DataStructures&Algos/practice.rb
+++ DataStructures&Algos/practice.rb
@@ -1,3 +1,21 @@
 def func
     #complete algo from structy                
 end
+
+def delete_duplicates(head)
+    
+    # take the head array and iterate over it and search for duplicates
+    # upon finding the duplicate numbers, remove them 
+    # if a node is removed be sure to reassign .next
+    # the linked list is already sorted, however we need maintain that order
+    # puts(head.next.next)
+    while head.next != nil
+        temp = head.next
+        if head.val == temp.val
+           head.next = temp.next
+        end 
+    end
+    
+    return head
+    
+end
