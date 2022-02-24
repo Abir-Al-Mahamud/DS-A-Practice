4bbe04aea0b98ec42ee8041cf10bea4c46f52868
your name here <abirmahamud610@gmail.com>
2022-02-24 12:56:58 -0500

    Work on algorithm

--- DataStructures&Algos/practice.js
+++ DataStructures&Algos/practice.js
@@ -79,3 +79,13 @@ const pairProduct = (numbers, targetProduct) => {
     }
 };
  
+
+const intersection = (a, b) => {
+    const result = [];
+    for (let item of b) {
+        if (a.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+};
