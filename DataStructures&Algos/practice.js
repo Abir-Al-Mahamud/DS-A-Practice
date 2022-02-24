5571faf9a779f2aeb7df7944dbbdd97c8b2b6741
your name here <abirmahamud610@gmail.com>
2022-02-24 12:54:07 -0500

    Work on algorithms

--- DataStructures&Algos/practice.js
+++ DataStructures&Algos/practice.js
@@ -20,3 +20,50 @@ const compress = (s) => {
   return result.join('');
 };
 
+const mostFrequentChar = (s) => {
+    const count = {};
+
+    for (let char of s) {
+        if (!(char in count)) {
+            count[char] = 0;
+        }
+        count[char] += 1
+    }
+
+    let best = null;
+    for (let char of s) {
+        if (best === null || count[char] > count[best]) {
+            best = char;
+        }
+    }
+    return best;
+};
+
+const pairSum = (numbers, targetSum) => {
+    const previousNums = {};
+    for (let i = 0; i < numbers.length; i += 1) {
+        const num = numbers[i];
+        const complement = targetSum - num;
+        if (complement in previousNums) return [i, previousNums[complement]];
+
+        previousNums[num] = i;
+    }
+};
+
+
+const anagrams = (s1, s2) => {
+    // todo
+    let hash1 = {}
+    for (let i = 0; i < s1.length; i += 1) {
+        hash1[s1[i]] += 1
+    }
+
+    let hash2 = {}
+    for (let i = 0; i < s1.length; i += 1) {
+        hash2[s2[i]] += 1
+    }
+
+    return hash1 === hash2
+};
+
+ 
