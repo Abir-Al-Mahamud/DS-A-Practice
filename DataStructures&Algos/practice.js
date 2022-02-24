189e4386dc221d0e191a54800698a3de0bf77e3b
your name here <abirmahamud610@gmail.com>
2022-02-24 12:54:50 -0500

    Work on algorithms

--- DataStructures&Algos/practice.js
+++ DataStructures&Algos/practice.js
@@ -66,4 +66,16 @@ const anagrams = (s1, s2) => {
     return hash1 === hash2
 };
  
+
+const pairProduct = (numbers, targetProduct) => {
+    const previousNums = {};
+    for (let i = 0; i < numbers.length; i += 1) {
+        const num = numbers[i];
+        const complement = targetProduct / num;
+
+        if (complement in previousNums) return [previousNums[complement], i];
+
+        previousNums[num] = i;
+    }
+};
  
