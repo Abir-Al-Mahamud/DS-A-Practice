61a317334fe82a0b167f87b4dc056b50a6972ed5
your name here <abirmahamud610@gmail.com>
2022-03-16 13:48:08 -0400

    Work on algorithm

--- DataStructures&Algos/practice.js
+++ DataStructures&Algos/practice.js
@@ -144,3 +144,59 @@ const fiveSort = (nums) => {
 
     return nums;
 };
+
+const sumList = (head) => {
+    let sum = 0;
+    let current = head;
+    while (current !== null) {
+        sum += current.val;
+        current = current.next;
+    }
+    return sum;
+};
+
+const linkedListFind = (head, target) => {
+    let current = head;
+    while (current !== null) {
+        if (current.val === target) return true;
+        current = current.next;
+    }
+    return false;
+};
+
+const reverseList = (head) => {
+    let current = head;
+    let prev = null;
+    while (current !== null) {
+        const next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+};
+
+const zipperLists = (head1, head2) => {
+    const head = head1;
+    let tail = head;
+    let current1 = head1.next;
+    let current2 = head2;
+    let count = 0;
+
+    while (current1 !== null && current2 !== null) {
+        if (count % 2 === 0) {
+            tail.next = current2;
+            current2 = current2.next;
+        } else {
+            tail.next = current1;
+            current1 = current1.next;
+        }
+        tail = tail.next;
+        count += 1;
+    }
+
+    if (current1 !== null) tail.next = current1;
+    if (current2 !== null) tail.next = current2;
+
+    return head;
+};
